803461846dda32e1ddc6fa698e550e54e0df06aa
Josiah Baumgartner <48170542+JosiahBaumgartner@users.noreply.github.com>
2019-07-07 22:32:20 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,22 +17,12 @@ FSJS project 2 - List Filter and Pagination
    scoped to that function.
 ***/
 
-const studentLi = document.querySelector(".student-list").children;
+const studentLi = document.querySelectorAll(".student-item");
 const studentsPerPage = 10;
 
 /***
    Create the `showPage` function to hide all of the items in the
    list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
 ***/
 function showPage(list, page) {
   const startIndex = (page * studentsPerPage) - studentsPerPage;
@@ -47,10 +37,8 @@ function showPage(list, page) {
   }
 }
 
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
+// Create the `appendPageLinks function` to generate, append, and add functionality to the pagination buttons.
+
 function appendPageLinks(list) {
 
   document.querySelector(".page").appendChild(document.createElement("div"));
@@ -60,8 +48,8 @@ function appendPageLinks(list) {
   div.appendChild(document.createElement("ul"));
   const ul = document.querySelector(".student-list").nextElementSibling.firstElementChild;
 
-// create for loop here to figure how many pages are needed and create links. pages = list.length/studentsPerPage. list argument for function is full list from index?
-  for (let i =0; i < list.length/studentsPerPage; i+=1){
+// Create for loop here to figure how many pages are needed and create links. pages = list.length/studentsPerPage.
+  for (let i = 0; i < list.length/studentsPerPage; i+=1){
 
   ul.appendChild(document.createElement("li"));
   const li = ul.getElementsByTagName("li");
